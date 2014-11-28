d0641841aa8ad77a25a25b1f7586f1efd585a9b6
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-11-28 08:20:17 +0000

    Complete exercises
    * Update integration test
    * Update helper test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    get signup_path
+    # Exercise - 2
+    # assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select "title", full_title("Sign up")
   end
 end
