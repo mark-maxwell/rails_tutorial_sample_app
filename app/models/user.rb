9a1e5e53a6ec9b68c68bd69a2a7e02b437abc866
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-12-02 11:59:22 +0000

    Validate user email format
    * Add tests for valid and invalid formatted emails
    * Add Regex to be used for validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 3, maximum: 50}
-  validates :email, presence: true, length: {minimum: 10, maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {minimum: 10, maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,26 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "Q" * 256
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid email addresses" do
+    valid_addresses = %w[test@example.com TEST@FOO.COM
+                       A_TEsT@foo.bar.org first.last@foo.jp
+                       test.test@baz.cn
+                        ]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid email addresses" do
+    invalid_addresses = %w[test@example,com test_at_foo.org
+                           test.name@example. foo@bar_baz.com
+                           foo@bar+baz.com
+                          ]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
