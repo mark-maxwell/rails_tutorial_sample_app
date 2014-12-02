6716295097838448c94ed20256d0e4cd77d66d97
Mark Maxwell <mark.maxwell@onthebeach.co.uk>
2014-12-02 09:52:10 +0000

    Add Validation
    * Check for presence of a minimum & maximum length for :name & :email
    * Update User spec to test this feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  validates :name, :email, presence: true
+  validates :name, presence: true, length: {minimum: 3, maximum: 50}
+  validates :email, presence: true, length: {minimum: 10, maximum: 255}
 end
